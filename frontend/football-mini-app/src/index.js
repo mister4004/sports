d74bc0328e11ba0d492f531b3d3878a6914639f3
mister4004 <nardin7n@gmail.com>
2025-03-11 02:48:59 +0500

    Fix resource paths for GitHub Pages

--- frontend/football-mini-app/src/index.js
+++ frontend/football-mini-app/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // Импортируем BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/sports"> {/* Добавляем basename */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
